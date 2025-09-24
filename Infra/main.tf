7a9cd0cbe783d6e8ba5911976d14738acd261b72
unknown <amiteshanand07@gmail.com>
2025-09-24 21:02:44 +0530

    adding storage account

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "rg_amitesh" {
   name     = "dev-rg"
   location = "Central india"
 }
+
+resource "azurerm_storage_account" "storage_amitesh" {
+  name                     = "storageaccountamitesh"
+  resource_group_name      = azurerm_resource_group.rg_amitesh.name
+  location                 = azurerm_resource_group.rg_amitesh.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
